ec63414ad2670897582b3ddb3510e40f5c4b8e17
Sun-G0D <47151235+Sun-G0D@users.noreply.github.com>
2025-01-25 19:59:49 -0800

    create main window
    create main window

---
+++ music_editor.py
@@ -0,0 +1,15 @@
+# Import Module
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Sonal")
+# Set geometry(widthxheight)
+root.geometry('350x200')
+
+
+
+# Execute Tkinter
+root.mainloop()
